4676d62652565990cb292ce326b3c34bbadeb73a
Benjamin Bädorf <b.baedorf@hosting.de>
2016-12-15 10:30:48 +0100

    Unified indentation

--- index.js
+++ index.js
@@ -11,54 +11,54 @@ var fs = require('fs');
  */
 
 var methods = [
-    'access',
-    'appendFile'
-    'chmod',
-    'chown',
-    'close',
-    'fchmod',
-    'fchown',
-    'fdatasync',
-    'fstat',
-    'fsync',
-    'ftruncate',
-    'futimes',
-    'lchmod',
-    'lchown',
-    'link',
-    'lstat',
-    'mkdir',
-    'mkdtemp',
-    'open',
-    'read',
-    'readFile',
-    'readdir',
-    'readlink',
-    'realpath',
-    'rename',
-    'rmdir',
-    'stat',
-    'symlink',
-    'truncate',
-    'unlink',
-    'utimes',
-    'write',
-    'writeFile',
+  'access',
+  'appendFile'
+  'chmod',
+  'chown',
+  'close',
+  'fchmod',
+  'fchown',
+  'fdatasync',
+  'fstat',
+  'fsync',
+  'ftruncate',
+  'futimes',
+  'lchmod',
+  'lchown',
+  'link',
+  'lstat',
+  'mkdir',
+  'mkdtemp',
+  'open',
+  'read',
+  'readFile',
+  'readdir',
+  'readlink',
+  'realpath',
+  'rename',
+  'rmdir',
+  'stat',
+  'symlink',
+  'truncate',
+  'unlink',
+  'utimes',
+  'write',
+  'writeFile',
 ];
 
 // wrap
 
 methods.forEach(function(name){
-    if (!fs[name]) return;
-    exports[name] = thunk(fs[name]);
+  if (!fs[name]) return;
+  exports[name] = thunk(fs[name]);
 });
 
 // .exists is still messed
 
 exports.exists = function(path){
-    return function(done){
-        fs.stat(path, function(err, res){
-            done(null, !err);
-        });
-    }
+  return function(done){
+    fs.stat(path, function(err, res){
+      done(null, !err);
+    });
+  }
 };
